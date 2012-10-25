6c23df77cff4a27841d40805e9a08af3350290f9
Joseph Bott <Joey@corvallistechnical.com>
2012-10-24 19:25:20 -0700

    First Commit

---
+++ gen/com/corvallistechnical/consumer/corvtech/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.corvallistechnical.consumer.corvtech;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
